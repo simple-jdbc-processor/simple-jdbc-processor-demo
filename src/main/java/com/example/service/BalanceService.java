6af1c2107ff81cbe3473326fb080f84c49c1b41b
simple-jdbc <simple-jdbc@gmail.com>
2026-01-14 18:07:39 +0800

    init

--- src/main/java/com/example/service/BalanceService.java
+++ src/main/java/com/example/service/BalanceService.java
@@ -6,19 +6,17 @@ import com.example.domain.BalanceExample;
 import com.example.repository.BalanceRepository;
 import io.github.simple.jdbc.processor.BaseService;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RequiredArgsConstructor
 @Slf4j
 @Service
 public class BalanceService extends BaseService<Balance, Long, BalanceExample> {
 
     private final BalanceRepository balanceRepository;
 
-    public BalanceService(BalanceRepository balanceRepository){
-        super(balanceRepository);
-        this.balanceRepository = balanceRepository;
-    }
 
 }
